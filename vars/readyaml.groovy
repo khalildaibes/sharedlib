36e9d9af9c6a354e27c58caa8482f8e70396b699
khalil daibes <kdaibes@seatgeek.com>
2023-03-28 16:25:40 +0300

    Update readyaml.groovy

--- vars/readyaml.groovy
+++ vars/readyaml.groovy
@@ -1,4 +1,5 @@
 def khaliltest(){
+def data = new URL ("https://raw.githubusercontent.com/khalildaibes/sharedlib/main/vars/datacenters.yml").getText()
 data = readYaml file: "datacenters.yml"
 scan_path = data[scan_path]
 scan_path.each { e ->
