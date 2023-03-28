1cecf39579a9aa0374aa5c5a599919dfb6edfe56
khalil daibes <kdaibes@seatgeek.com>
2023-03-28 15:51:41 +0300

    Update readyaml.groovy

--- vars/readyaml.groovy
+++ vars/readyaml.groovy
@@ -1,9 +1,8 @@
-mydata = readYaml file: "test.yml"
-
-// modify
+def readyaml(){
 data = readYaml file: "datacenters.yml"
 scan_path = data[scan_path]
 scan_path.each { e ->
                 echo "Translating ${e.getAt('data-centers')} application"
+}
 }
 
