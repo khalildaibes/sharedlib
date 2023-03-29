c71fd2ef8336d6f8c2761fea273109a69c0391d9
khalil daibes <kdaibes@seatgeek.com>
2023-03-29 13:06:02 +0300

    Update readyaml.groovy

--- vars/readyaml.groovy
+++ vars/readyaml.groovy
@@ -1,9 +1,11 @@
 def get_data_centers(jobname){
   def configVal = readYaml file: "datacenters.yml"
-  def datacenters = configVal['datacenters']["${jobname}"]["data-centers"] 
-  def x = datacenters.find{ it == "${jobname}" }?.value
-  if(x)
-    ehco "x value: ${x}"
+  if(configVal['datacenters'].containsKey("${jobname}")){
+  def datacenters = configVal['datacenters']["${jobname}"]["data-centers"]
+  }
+  else {
+    return [""]
+  }
   return datacenters
 }
 
