b867bea6d95eb350dc04cb3cea0a4db9a6ad96f4
khalil daibes <kdaibes@seatgeek.com>
2023-03-29 10:33:50 +0300

    Update readyaml.groovy

--- vars/readyaml.groovy
+++ vars/readyaml.groovy
@@ -1,6 +1,6 @@
 def khaliltest(){
   def configVal = readYaml file: "datacenters.yml"
   echo "configVal: " + configVal
-  echo configVal['datacenterc']["compliance"]["data-centers"]
+  echo configVal['datacenterc']["compliance"]["data-centers"][0]
 }
 
