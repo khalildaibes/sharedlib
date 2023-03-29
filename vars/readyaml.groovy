f94fb530a0414a929832effc79fabdc974f79d66
khalil daibes <kdaibes@seatgeek.com>
2023-03-29 13:36:01 +0300

    Update readyaml.groovy

--- vars/readyaml.groovy
+++ vars/readyaml.groovy
@@ -2,13 +2,11 @@ def get_data_centers(jobname){
   def configVal = readYaml file: "datacenters.yml"
   def datacenters= [""]
   try{
-
       datacenters = configVal['datacenters']["${jobname}"]["data-centers"]
     }
-    }catch(Exception e){
-
+    catch(Exception e){
         echo 'Job dosent support datacenters'
-    return [""]
+        return [""]
     }
   return datacenters
 }
