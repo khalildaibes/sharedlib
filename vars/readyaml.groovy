9f3aa45d5246c5cb752c1a600d02884e92f3a3fe
khalil daibes <kdaibes@seatgeek.com>
2023-03-29 13:26:56 +0300

    Update readyaml.groovy

--- vars/readyaml.groovy
+++ vars/readyaml.groovy
@@ -2,7 +2,7 @@ def get_data_centers(jobname){
   def configVal = readYaml file: "datacenters.yml"
   echo configVal['datacenters'].getClass().toString()
   HashMap h = new HashMap(configVal['datacenters']);
-  if (h.containsKey(jobname.toString()){
+  if (h.containsKey(jobname.toString())){
   def datacenters = configVal['datacenters']["${jobname}"]["data-centers"]
   }
       else {
