98b3d9e13e45ca51649086f2d58c3ab2fe377ef3
khalil daibes <kdaibes@seatgeek.com>
2023-03-29 11:40:58 +0300

    Update readyaml.groovy

--- vars/readyaml.groovy
+++ vars/readyaml.groovy
@@ -1,4 +1,4 @@
-def khaliltest(jobname){
+def get_data_centers(jobname){
   def configVal = readYaml file: "datacenters.yml"
   def datacenters = configVal['datacenters']["${jobname}"]["data-centers"]
   return datacenters
