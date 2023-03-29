bd341b747e02fc266c09bdda9dbe0c94b3ce7fad
khalil daibes <kdaibes@seatgeek.com>
2023-03-29 12:49:08 +0300

    Update readyaml.groovy

--- vars/readyaml.groovy
+++ vars/readyaml.groovy
@@ -1,13 +1,6 @@
 def get_data_centers(jobname){
   def configVal = readYaml file: "datacenters.yml"
-  echo configVal['datacenters'].getClass().toString()
-  echo jobname
-  if (configVal['datacenters'].containsKey('${jobname}')){
-    def datacenters = configVal['datacenters']["${jobname}"]["data-centers"]
-  }else{
-    echo "Job isn't supported"
-    return [""]
-  }
+  def datacenters = configVal['datacenters']["${jobname}"]["data-centers"] ?: echo "Job isn't supported"
   return datacenters
 }
 
