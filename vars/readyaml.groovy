0df478d6e3a93cc179ebbc2c1584176c6e8a4019
khalil daibes <kdaibes@seatgeek.com>
2023-03-29 15:12:32 +0300

    Update readyaml.groovy

--- vars/readyaml.groovy
+++ vars/readyaml.groovy
@@ -1,3 +1,5 @@
+import groovyx.net.http.ContentType
+
 def get_data_centers(jobname){
   def configVal = readYaml file: "datacenters.yml"
   def datacenters= [""]
@@ -12,3 +14,36 @@ def get_data_centers(jobname){
   return datacenters
 }
 
+
+def get_db_type(custoemr){
+  def db_type = "Null"
+  json_result = cops_api(customer)
+return db_type  
+}
+
+def cops_api(authToken,customer) {
+  http.request(POST) {
+      uri.path = 'http://cops.onbmc.com/cops/api.php'
+      body = {
+    "jsonrpc": "2.0",
+    "method": "getprojectparams",
+    "params": {
+        "project": "refash1np02",
+        "filter": "/DB_TYPE/"
+    },
+    "id": "getprojectparams"
+}
+
+      requestContentType = ContentType.JSON
+
+      response.success = { resp, json ->
+          echo "Success! ${resp.status}"
+          echo json.toString()
+      }
+
+      response.failure = { resp ->
+          println "Request failed with status ${resp.status}"
+      }
+  }
+	return json
+}
