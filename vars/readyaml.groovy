65563f33e5fa9a29d9a4dad54d86ac1a4e72e6ea
khalil daibes <kdaibes@seatgeek.com>
2023-04-03 13:16:44 +0300

    Update readyaml.groovy

--- vars/readyaml.groovy
+++ vars/readyaml.groovy
@@ -14,7 +14,9 @@ import static groovyx.net.http.Method.*
 def get_data_centers(jobname){
   def filecontent = libraryResource('datacenters.yml')
   File file = File.createTempFile("temp",".yml")
-  file.write filecontent
+  echo $?
+  RESULT = file.write filecontent
+  echo "${RESULT}"
   def configVal = readYaml  file: file.absolutePath
   try{
       datacenters = configVal['datacenters']["${jobname}"]["data-centers"]
