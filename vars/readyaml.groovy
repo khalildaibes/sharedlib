4c52e94d81f985ccf86ee6fcc1172ad893dab130
khalil daibes <kdaibes@seatgeek.com>
2023-04-03 13:15:02 +0300

    Update readyaml.groovy

--- vars/readyaml.groovy
+++ vars/readyaml.groovy
@@ -1,7 +1,19 @@
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.1')
+
+import groovyx.net.http.HTTPBuilder
+//import groovyx.net.http.ContentType // this doesn't import ContentType
+//import groovyx.net.http.Method // this doesn't import Method
+import groovyx.net.http.RESTClient
+import groovyx.net.http.HttpResponseDecorator
+
+// ContentType static import
+import static groovyx.net.http.ContentType.*
+// Method static import
+import static groovyx.net.http.Method.*
+
 def get_data_centers(jobname){
   def filecontent = libraryResource('datacenters.yml')
-  echo filecontent
-  File file = File.createTempFile("temp_${currentBuild.number}",".yml")
+  File file = File.createTempFile("temp",".yml")
   file.write filecontent
   def configVal = readYaml  file: file.absolutePath
   try{
@@ -28,16 +40,21 @@ def body = [
    jsonrpc= "2.0",method= "getprojectparams", params= [project= "refash1np02",filter='/DB_TYPE/'],id= "getprojectparams"
 ]
 def post = new URL("http://cops.onbmc.com/cops/api.php").openConnection();
-post.setRequestMethod("POST")
-post.setDoOutput(true)
-post.setRequestProperty("Content-Type", "application/json")
-post.setRequestProperty("APIKEY", "24df72c4a77c436a8195e0949fa3868a")
-post.getOutputStream().write(body.getBytes("UTF-8"));
-post.connect();
-def postRC = post.getResponseCode();
-println(postRC);
-if (postRC.equals(200)) {
-    echo(post.getInputStream().getText());
+def url = "http://cops.onbmc.com/cops/api.php"
+def requestBody = [jsonrpc= "2.0",method= "getprojectparams", params= [project= "refash1np02",filter="""/DB_TYPE/"""],id= "getprojectparams"]
+
+def http = new HTTPBuilder(url)
+
+http.request(POST, "applicaiton/json") { req ->
+  body = requestBody
+  response.success = { resp, json ->
+    println "Success: ${resp.statusLine}"
+    println "Response body: ${json}"
+  }
+  response.failure = { resp, json ->
+    println "Failure: ${resp.statusLine}"
+    println "Response body: ${json}"
+  }
 }
 echo result
 }
